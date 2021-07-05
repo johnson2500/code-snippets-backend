23b0195b5848115949fe1f933800ca546417135a
Ryan Johnson <ryanjohnson@Ryans-MacBook-Pro.local>
2021-07-05 18:39:19 -0400

    fixing port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
