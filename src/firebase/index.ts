890546a57480295b66d61732c9978b4bb844171f
Ryan Johnson <johnson2500@live.com>
2022-07-19 21:20:09 -0400

    fix

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,6 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import dotenv from 'dotenv';
 import config from './config';
-
-dotenv.config();
 
 const admin = initializeApp({ credential: cert(config()) });
 
